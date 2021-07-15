3b954d2ef372c141028b18de87ac97b8fef9a4a0
unknown <vikramsuryawanshi619.vs@gmail.com>
2021-07-15 15:54:39 +0530

    calendly added

--- src/components/Homepage/Chatbot.js
+++ src/components/Homepage/Chatbot.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import './chatbot.css'
-import { Modal, Button } from "react-bootstrap";
+// import { Modal, Button } from "react-bootstrap";
+import { PopupWidget } from "react-calendly";
 
 export default class Chatbot extends Component {
-    state={
-        isOpen:false
-    }
+    // state={
+    //     isOpen:false
+    // }
 
-    openModal = () => this.setState({ isOpen: true });
-      closeModal = () => this.setState({ isOpen: false })
+    componentDidMount() {
+        const head = document.querySelector('head');
+        const script = document.createElement('script');
+        script.setAttribute('src',  'https://assets.calendly.com/assets/external/widget.js');
+        head.appendChild(script);
+    }
+    
+
+    // openModal = () => this.setState({ isOpen: true });
+    //   closeModal = () => this.setState({ isOpen: false })
 
     render() {
         return (
@@ -23,12 +32,11 @@ export default class Chatbot extends Component {
                 </div>
 
                 <div className="chatbot_requestdemo_div">
-                <Button className="footer_subscribe_button" variant="primary" onClick={this.openModal}>
-                                    {/* <img alt="" src={mail} className="social_icon mail"></img> */}
-                                    Request Demo
-                                        </Button>
+                                    {/* <Button className="footer_subscribe_button" variant="primary" onClick={this.openModal}>
+                                        Request Demo
+                                    </Button>
 
-                                        <Modal show={this.state.isOpen} onHide={this.closeModal} animation={false} 
+                                    <Modal show={this.state.isOpen} onHide={this.closeModal} animation={false} 
                                             backdrop="static" dialogClassName="modal-60w">
                                         <Modal.Header closeButton className="close_button_request_demo">
                                             <Modal.Title>Request Demo</Modal.Title>
@@ -36,12 +44,26 @@ export default class Chatbot extends Component {
 
                                         <Modal.Body>
                                             <input className="input" placeholder="Business email address to start saving"></input>
-                                            {/* <input className="input" type="area" placeholder="Enter you"></input> */}
                                             <Button variant="primary" className="modal_buttons modal_submit_button">
                                                 Submit
                                             </Button>
                                         </Modal.Body>
-                                    </Modal>
+                                    </Modal> */}
+                                    <PopupWidget  url="https://calendly.com/bizgam/effybuy"
+                                    //   text="Request Demo"
+                                    //   pageSettings={{
+                                    //     backgroundColor: 'ffffff',
+                                    //     hideEventTypeDetails: false,
+                                    //     hideLandingPageDetails: false,
+                                    //     primaryColor: '008ed9',
+                                    //     textColor: '4d5055'
+                                    //   }}
+                                    //   color="red"
+                                       />
+
+
+
+                                    
                 </div>
             </div>
         )
